c98e485458ad714b696f612933d02dfa592d0b78
sas4211 <sas4211@gmail.com>
2024-11-16 13:45:57 +0500

    Finalizing

---
+++ src/containers/providers.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ActiveSectionContextProvider from "./active-section";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "react-hot-toast";
+import ThemeContextProvider from "./theme-context";
+
+
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeContextProvider>
+        <ActiveSectionContextProvider>
+          <Analytics />
+          <Toaster />
+          {children}
+        </ActiveSectionContextProvider>
+      </ThemeContextProvider>
+  );
+}
