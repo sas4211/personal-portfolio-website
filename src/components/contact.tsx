5b5ab6d189ebc04a96a1580558ecdac78f8d858e
sas4211 <160629359+sas4211@users.noreply.github.com>
2024-11-28 12:12:30 +0400

    Update contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import SubmitBtn from "./submit-btn";
